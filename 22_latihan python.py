984b41dbf3176ad588487e43b681cc4e838d2926
RafifAlvis45 <rafifalvis.tkjsmeksa25@gmail.com>
2026-08-03 19:30:43 +0700

    Add files via upload

---
+++ 22_latihan python.py
@@ -0,0 +1,10 @@
+"""
+Program Menghitung Luas Persegi Panjanng
+"""
+
+Panjang = float(input("Masukkan Panjang: "))
+Lebar = float(input("Masukkan Lebar: "))
+
+Luas = Panjang * Lebar
+
+print(f"Luas Persegi Panjang dengan Panjang {Panjang} dan Lebar {Lebar} adalah {Luas}")
